772a0dff821cd611f8b12dd294836bb794126af0
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-29 05:09:10 +0000

    feat: vector DB 생성 기능 구현

--- demo/vector_search.py
+++ demo/vector_search.py
@@ -10,3 +10,17 @@ def search_process(prompt_embeds):
     database = load_vector_database()
     prompt_embeds = prompt_embeds.flatten()
     return database.get_nns_by_vector(prompt_embeds, SEARCH_SIZE)
+
+def create_vector_database():
+    database = AnnoyIndex(VECTOR_SIZE, SIMILARITY)
+    pipeline = get_pipeline(get_clip_model(), get_clip_processor())
+
+    for idx, prompt_description in enumerate(prompt_descriptions):
+        generator = torch.manual_seed(SEEDS[idx])
+
+        (prompt_embeds, _) = pipeline.encode_prompt(generate_prompt, "cuda", num_images_per_prompt=1, do_classifier_free_guidance=False)
+        prompt_embeds = prompt_embeds.flatten()
+        annoy_index.add_item(idx, prompt_embeds)
+
+    annoy_index.build(10)
+    annoy_index.save('hanbok.annoy')
