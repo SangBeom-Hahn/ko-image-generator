ca7dbcd34c155235e38931f803bbd04d83ceee49
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-29 00:57:24 +0000

    feat: 라이브러리 추가

---
+++ demo/app.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+from flask import Flask, render_template, request, url_for,
+from transformers import CLIPTextModel, CLIPTokenizer
+from diffusers import DiffusionPipeline, StableDiffusionXLImg2ImgPipeline, StableDiffusionImg2ImgPipeline
+from diffusers.utils import load_image
+from typing import Final
+from annoy import AnnoyIndex
+import torch, random
